9f1cef136d3cf442a20d512362b677c725256a7e
LieT233 <f020628@gmail.com>
2023-11-05 14:43:22 -0500

    shuffle

--- Assets/Scripts/Monster/DeckManager.cs
+++ Assets/Scripts/Monster/DeckManager.cs
@@ -20,6 +20,7 @@ public class DeckManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.L))
         {
             monster.ShuffleCards(); // Shuffle the deck
+            StartCoroutine(ShuffleDeckVisual()); // Trigger the shuffle animation
         }
     }
      private void DrawNewActionCard()
@@ -30,8 +31,8 @@ public class DeckManager : MonoBehaviour
 
             // Instantiate the card game object
             GameObject cardObject = Instantiate(cardPrefab, this.transform.position, Quaternion.identity);
-            cardObject.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-         
+            cardObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            //cardObject.transform.SetParent(cardParent, false); // 确保牌的父对象是cardParent
 
             // Set the card data on the newly created card object
             ActionCardDisplay cardDisplay = cardObject.GetComponent<ActionCardDisplay>();
@@ -43,12 +44,14 @@ public class DeckManager : MonoBehaviour
         }
         else
         {
-            Debug.Log("Deck is empty, shuffling...");
-            monster.ShuffleCard(); // Shuffle the deck if empty before drawing a card
             if (currentDisplayedCard != null)
             {
                 Destroy(currentDisplayedCard);
             }
+            Debug.Log("Deck is empty, shuffling...");
+            monster.ShuffleCard(); // 洗牌逻辑
+            StartCoroutine(ShuffleDeckVisual()); // 触发洗牌动画
+            
         }
     }
 
@@ -68,5 +71,41 @@ public class DeckManager : MonoBehaviour
         }
         currentDisplayedCard = card;
     }
-}
+    private IEnumerator ShuffleDeckVisual()
+    {
+        GameObject cardBack1 = Instantiate(cardPrefab, drawPoint.position, Quaternion.identity);
+        cardBack1.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); // 设置卡片的大小
+        //rotate Z 180
+        cardBack1.transform.Rotate(0, 0, 180);
+        for (int i = 0; i < monster.actionCardsDeck.Length - 1; i++)
+        {
+            GameObject cardBack = Instantiate(cardPrefab, drawPoint.position, Quaternion.identity);
+            cardBack.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); // 设置卡片的大小
+            //rotate Z 180
+            cardBack.transform.Rotate(0, 0, 180);
+            // cardBack.transform.SetParent(cardParent, false); // 如果需要，可以取消注释
 
+            // Move each card to the deck one by one with a delay between them
+            yield return StartCoroutine(MoveCardToDeck(cardBack, cardParent.position, 0.4f));
+            
+            // Wait for a short delay before moving the next card
+            yield return new WaitForSeconds(0.2f);
+        }
+        yield return StartCoroutine(MoveCardToDeck(cardBack1, cardParent.position, 0.4f));
+        Destroy(cardBack1);
+    }
+
+    private IEnumerator MoveCardToDeck(GameObject card, Vector3 newPosition, float timeToMove)
+    {
+        var currentPos = card.transform.position;
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            card.transform.position = Vector3.Lerp(currentPos, newPosition, t);
+            yield return null;
+        }
+        Destroy(card); // Destroy the card after it has moved to the deck
+    }
+    
+}
